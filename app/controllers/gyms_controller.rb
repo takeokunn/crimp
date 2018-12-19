8fcaa2a5d95dcd3055a83e1308d5d986c8dffa26
HIROKI INOUE <hiroki@HIROKInoMacBook-puro.local>
2018-12-19 13:48:29 +0900

    make calculate rank defs

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -10,9 +10,8 @@ class GymsController < ApplicationController
   def show
     @posts = Post.where(gym_id: params[:id])
     @home_gym = HomeGym.where(gym_id: params[:id])
-    @gym_likes = GymLike.where(gym_id: params[:id])
-    overall_gym_review
-    calculate_gym_rank if @overall_gym_review
+		@gym_likes = GymLike.where(gym_id: params[:id])
+		overall_gym_review
   end
 
   def edit; end
@@ -43,43 +42,4 @@ class GymsController < ApplicationController
 		@comfortableness = @overall_gym_review.comfortableness
 		@service = @overall_gym_review.service
 	end
-
-	def calculate_gym_rank
-		@overall_gym_reviews = OverallGymReview.all
-		calculate_overall_score_rank
-		calculate_problems_quality_rank
-		calculate_cost_par_fee_rank
-		calculate_comfortableness_rank
-		calculate_service_rank
-		end
-
-	def calculate_overall_score_rank
-		@overall_score_rank = 1
-		@overall_score_rank += 1 while @overall_gym_reviews.order(overall_score: :desc).pluck(:overall_score)[@overall_score_rank - 1] > OverallGymReview.find_by(gym_id: params[:id]).overall_score if @overall_gym_reviews.length >= 2
-	end
-
-	def calculate_problems_quality_rank
-		@overall_problems_quality_rank = 1
-		@overall_gym_reviews_of_problems_quality = @overall_gym_reviews.order(problems_quality: :desc)
-		@overall_problems_quality_rank += 1 while @overall_gym_reviews_of_problems_quality.pluck(:problems_quality)[@overall_problems_quality_rank - 1] > OverallGymReview.find_by(gym_id: params[:id]).problems_quality if @overall_gym_reviews.length >= 2
-	end
-
-	def calculate_cost_par_fee_rank
-		@overall_cost_par_fee_rank = 1
-		@overall_gym_reviews_of_cost_par_fee = @overall_gym_reviews.order(cost_par_fee: :desc)
-		@overall_problems_quality_rank += 1 while @overall_gym_reviews_of_cost_par_fee.pluck(:cost_par_fee)[@overall_cost_par_fee_rank - 1] > OverallGymReview.find_by(gym_id: params[:id]).cost_par_fee if @overall_gym_reviews.length >= 2
-	end
-
-	def calculate_comfortableness_rank
-		@overall_comfortableness_rank = 1
-		@overall_gym_reviews_of_comfortableness = @overall_gym_reviews.order(comfortableness: :desc)
-		@overall_comfortableness_rank += 1 while @overall_gym_reviews_of_comfortableness.pluck(:comfortableness)[@overall_comfortableness_rank - 1] > OverallGymReview.find_by(gym_id: params[:id]).comfortableness if @overall_gym_reviews.length >= 2
-	end
-
-	def calculate_service_rank
-		@overall_service_rank = 1
-		@overall_gym_reviews_of_service = @overall_gym_reviews.order(service: :desc)
-		@overall_service_rank += 1 while @overall_gym_reviews_of_service.pluck(:service)[@overall_service_rank - 1] > OverallGymReview.find_by(gym_id: params[:id]).service if @overall_gym_reviews.length >= 2
-	end
-
 end
--- app/models/overall_gym_review.rb
+++ app/models/overall_gym_review.rb
@@ -5,9 +5,40 @@ class OverallGymReview < ApplicationRecord
 
 
 	def calculate_overall_score_rank
-		@overall_scores = OverallGymReview.all.order(overall_score: :desc).pluck(:overall_score)
+		overall_scores = OverallGymReview.all.order(overall_score: :desc).pluck(:overall_score)
 		overall_score_rank = 1
-		overall_score_rank += 1 while @overall_scores[@overall_score_rank - 1] > self.overall_score
+		overall_score_rank += 1 while overall_scores[overall_score_rank - 1] > self.overall_score
 		return overall_score_rank
 	end
+
+	def calculate_problems_quality_rank
+		overall_problems_qualities = OverallGymReview.all.order(problems_quality: :desc).pluck(:problems_quality)
+		rank = 1
+		rank += 1 while overall_problems_qualities[rank - 1] > self.problems_quality
+		return rank
+	end
+
+	def calculate_cost_par_fee_rank
+		overall_cost_par_fees = OverallGymReview.all.order(cost_par_fee: :desc).pluck(:cost_par_fee)
+		cost_par_fee_rank = 1
+		cost_par_fee_rank += 1 while overall_cost_par_fees[cost_par_fee_rank - 1] > self.cost_par_fee
+		return cost_par_fee_rank
+	end
+
+	def calculate_comfortableness_rank
+		overall_comfortablenesses = OverallGymReview.all.order(comfortableness: :desc).pluck(:comfortableness)
+		comfortableness_rank = 1
+		comfortableness_rank += 1 while overall_comfortablenesses[comfortableness_rank - 1] > self.comfortableness
+		return comfortableness_rank
+	end
+
+	def calculate_service_rank
+		overall_servicies = OverallGymReview.all.order(service: :desc).pluck(:service)
+		service_rank = 1
+		service_rank += 1 while overall_servicies[service_rank - 1] > self.service
+		return service_rank
+	end
+
+
+
 end
