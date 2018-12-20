b8a2d75442ca92884ea6119973b87298c10b9785
HIROKI INOUE <hiroki@HIROKInoMacBook-puro.local>
2018-12-20 10:55:57 +0900

    add validates model gym_review

--- app/models/gym_review.rb
+++ app/models/gym_review.rb
@@ -2,5 +2,10 @@
 
 class GymReview < ApplicationRecord
   belongs_to :user
-  belongs_to :gym
+	belongs_to :gym
+	validates :overall_score, presence: true
+	validates :problems_quality, presence: true
+	validates :cost_par_fee, presence: true
+	validates :comfortableness, presence: true
+	validates :service, presence: true
 end
