65c38f376dedc83520312e07a85597602d821b8c
HIROKI INOUE <hiroki@HIROKInoMacBook-puro.local>
2019-01-05 18:22:16 +0900

    change gym_reviews_controller

--- app/controllers/gym_reviews_controller.rb
+++ app/controllers/gym_reviews_controller.rb
@@ -3,14 +3,18 @@ class GymReviewsController < ApplicationController
     @gym_review = GymReview.new(gym_id: params[:gym_id], user_id: current_user.id)
   end
 
-  def create
-    @gym_review = GymReview.create(params.require(:gym_review).permit(:problems_quality, :cost_par_fee, :comfortableness, :service))
-    @gym_review.overall_score = (@gymReview.problems_quality + @gymReview.cost_par_fee + @gymReview.comfortableness + @gymReview.service) / 4
-    @gym_review.user_id = current_user.id
-    @gym_review.gym_id = params[:gym_id]
-    @gym_review.save
-    calculate_gym_ranking
-    redirect_to gym_path(params[:gym_id])
+	def create
+		@gym_review = GymReview.create(params.require(:gym_review).permit(:problems_quality, :cost_par_fee, :comfortableness, :service))
+    if @gym_review.valid?
+			@gym_review.overall_score = (@gymReview.problems_quality + @gymReview.cost_par_fee + @gymReview.comfortableness + @gymReview.service) / 4
+			@gym_review.user_id = current_user.id
+			@gym_review.gym_id = params[:gym_id]
+			@gym_review.save
+			calculate_gym_ranking
+			redirect_to gym_path(params[:gym_id])
+		else
+			redirect_to root_path
+		end
   end
 
   def destroy; end
@@ -19,13 +23,17 @@ class GymReviewsController < ApplicationController
     @gym_review = GymReview.find_by(gym_id: params[:gym_id], user_id: current_user.id)
   end
 
-  def update
-    @gym_review = GymReview.find_by(gym_id: params[:gym_id], user_id: current_user.id)
-    @gym_review.update(params.require(:gym_review).permit(:problems_quality, :cost_par_fee, :comfortableness, :service))
-    @gym_review.overall_score = (@gym_review.problems_quality + @gym_review.cost_par_fee + @gym_review.comfortableness + @gym_review.service) / 4
-    @gym_review.save
-    calculate_gym_ranking
-    redirect_to gym_path(params[:gym_id])
+	def update
+		@gym_review = GymReview.find_by(gym_id: params[:gym_id], user_id: current_user.id)
+    if @gym_review.valid?
+			@gym_review.update(params.require(:gym_review).permit(:problems_quality, :cost_par_fee, :comfortableness, :service))
+			@gym_review.overall_score = (@gym_review.problems_quality + @gym_review.cost_par_fee + @gym_review.comfortableness + @gym_review.service) / 4
+			@gym_review.save
+			calculate_gym_ranking
+			redirect_to gym_path(params[:gym_id])
+		else
+			redirect_to root_path
+		end
   end
 
   private
--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class PostLikesController < ApplicationController
-  def create
-    PostLike.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_back(fallback_location: gym_path(params[:gym_id]))
+	def create
+    if PostLike.create(user_id: current_user.id, post_id: params[:post_id])
+			redirect_back(fallback_location: gym_path(params[:gym_id]))
+		else
+			redirect_to root_path
+		end
   end
 
   def destroy
-    PostLike.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
-    redirect_back(fallback_location: gym_path(params[:gym_id]))
+    if PostLike.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
+			redirect_back(fallback_location: gym_path(params[:gym_id]))
+		else
+			redirect_to root_path
+		end
   end
 end
